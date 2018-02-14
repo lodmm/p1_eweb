1efeb56aff11b829dbd5a08e92ad86aef799fdee
Lorena <lorenadmm@gmail.com>
2018-02-14 15:13:05 +0100

    14feb

--- P1/indexer.py
+++ P1/indexer.py
@@ -1,5 +1,3 @@
-# indexer
-
 import sys
 import json
 import nltk
@@ -14,81 +12,65 @@ import string
 stop = stopwords.words('english') + list(string.punctuation)
 tknzr = TweetTokenizer(preserve_case=False,reduce_len=True,strip_handles=True)
 
-indexer = list()
-num = []
 moocs='.\corpora\moocs\json\moocs.json'
 cf=[".\corpora\cf\json\cf74.json", ".\corpora\cf\json\cf75.json", ".\corpora\cf\json\cf76.json", ".\corpora\cf\json\cf77.json", ".\corpora\cf\json\cf78.json", ".\corpora\cf\json\cf79.json"]
-cnt = Counter()
-data = []
-f = list()
+dict = dict()
+id = {}
+data = {}
 jcf = []
-d_list = list()
+str3 =""
 if len(sys.argv) != 3:
 	print ("Error: missing corpus, indexer -c [moocs|cf]")
 	sys.exit(1)
 if (sys.argv[1]+" "+sys.argv[2]) == "-c moocs" :
 	# indexer for moocs
 	jmoocs = json.loads(open(moocs).read())
-	# print (len(jmoocs))
 	for x in jmoocs:
 		dt = tknzr.tokenize(x["description"] + x["title"])
 		dt = [i for i in dt if i not in stop]
-		# print (len(dt))
 		for y in dt:
-			cnt[y] +=1
-			if y not in d_list:
-				d_list.append(y)
-				
-		for y in d_list :
-			doc_id=([x["courseID"],cnt[y]])
-			if y not in f:
-				f.append(y)
-				data.append({
-				'word':y,
-				'docs': [
-				[x["courseID"],cnt[y]]
-				]
-				})
+			if y not in dict:
+				id[x["courseID"]] = 1
+				dict[y] = id.copy()	
 			else:
-				for w in data:
-					if w['word'] == y:
-						w['docs'].append([x["courseID"],cnt[y]])		
-		cnt.clear()
-		d_list.clear()
+				data = dict.get(y)
+				if x["courseID"] in data:
+					data[x["courseID"]] = data.get(x["courseID"]) +1
+				else:
+					data[x["courseID"]] = 1
+				dict[y] = data.copy()	
 				
+		id.clear()
+		data.clear()	
 	with open('.\indices\moocs_indexer.dat', 'w') as outfile:
-		json.dump(data, outfile)
+		json.dump(dict.sort(), outfile)
 elif (sys.argv[1]+" "+sys.argv[2] ) == "-c cf" :
 	# indexer for cf
 	for c in cf:
-		jcf += json.loads(open(c).read())
-	
-	for x in jcf:
-		dt = tknzr.tokenize( x["abstract/extract"] )
-		dt = [i for i in dt if i not in stop]	
-		
-		for y in dt:
-			cnt[y] +=1
-			if y not in d_list:
-				d_list.append(y)	
-		for y in d_list :
-			if y not in f:
-				f.append(y)
-				data.append({
-				'word':y,
-				'docs': [
-				[x["paperNum"],cnt[y]]
-				]
-				})
-			else:
-				#get the w in data for word=y
-				
-				for w in data:
-					if w['word'] == y:
-						w['docs'].append([x["paperNum"],cnt[y]])		
-		cnt.clear
-		d_list.clear()
-	print(data)	
+		jcf = json.loads(open(c).read())
+		for x in jcf:
+			str2= x["minorSubjects"] 
+			str1 = x.get('majorSubjects')
+			str3 = ' '.join(str3)
+			str =  ' '.join(str2)
+			dt = tknzr.tokenize( x["abstract/extract"] + x["title"] + str + str3)
+			dt = [i for i in dt if i not in stop]	
+			for y in dt:
+				if y not in dict:
+					id[x["paperNum"]] = 1
+					dict[y] = id.copy()	
+				else:
+					data = dict.get(y)
+					if x["paperNum"] in data:
+						data[x["paperNum"]] = data.get(x["paperNum"]) +1
+					else:
+						data[x["paperNum"]] = 1
+					dict[y] = data.copy()	
+					
+		id.clear()
+		data.clear()	
+	with open('.\indices\cf_indexer.dat', 'w') as outfile:
+		json.dump(dict, outfile)	
 else:
 	print ("Error: introduce a valid corpus, indexer -c [moocs|cf]")
 	sys.exit(1)
